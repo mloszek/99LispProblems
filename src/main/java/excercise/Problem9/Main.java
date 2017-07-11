be84b825d8916a736a6017e7f243ae03cc3d3675
mloszek <miloszloszek@gmail.com>
2017-07-11 21:41:13 +0200

    next_commit; up to problem 9;

---
+++ src/main/java/excercise/Problem9/Main.java
@@ -0,0 +1,18 @@
+package excercise.Problem9;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Packing consecutive duplicates of list elements into sublists
+ */
+public class Main {
+    public static void main(String[] args){
+
+        List<Integer> list = Arrays.asList(1, 2, 2, 5, 3, 7, 7, 3, 3, 3, 3, 8, 6, 6);
+
+
+
+
+    }
+}
