37b76b7a70438330aba8b96af46075e9dd73ba02
mloszek <miloszloszek@gmail.com>
2017-07-12 13:48:16 +0200

    next_commit;

--- src/main/java/excercise/Problem11/Main.java
+++ src/main/java/excercise/Problem11/Main.java
@@ -10,28 +10,26 @@ import java.util.List;
  * Modified run-length encoding
  */
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         List<List<Integer>> list = Arrays.asList(Arrays.asList(1), Arrays.asList(2, 2), Arrays.asList(3), Arrays.asList(7, 7), Arrays.asList(3, 3, 3, 3), Arrays.asList(8), Arrays.asList(6, 6));
 
         List<excercise.Problem10.IntegersEncoder> encodedList = new ArrayList<>();
 
-        for (List<Integer> numbers: list) {
+        for (List<Integer> numbers : list) {
             encodedList.add(new excercise.Problem10.IntegersEncoder(numbers.size(), numbers.get(0).toString()));
-        }
+        }//Problem 10 code
 
         List<Object> modList = new ArrayList<>();
 
-        for (IntegersEncoder integersEncoder: encodedList) {
-            if (integersEncoder.getAmount() == 1){
-              modList.add(integersEncoder.getCharacter());
-            }else
+        for (IntegersEncoder integersEncoder : encodedList) {
+            if (integersEncoder.getAmount() == 1) {
+                modList.add(integersEncoder.getCharacter());
+            } else
                 modList.add(integersEncoder);
         }
 
         modList.forEach(element -> System.out.println(element.toString()));
-
-
 
     }
 }
--- src/main/java/excercise/Problem12/Main.java
+++ src/main/java/excercise/Problem12/Main.java
@@ -1,7 +1,34 @@
 package excercise.Problem12;
 
+import excercise.Problem10.IntegersEncoder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * Created by milosz on 7/12/17.
+ * Decoding a run-length encoded list
  */
 public class Main {
+    public static void main(String[] args) {
+
+        List<List<Integer>> list = Arrays.asList(Arrays.asList(1), Arrays.asList(2, 2), Arrays.asList(3), Arrays.asList(7, 7), Arrays.asList(3, 3, 3, 3), Arrays.asList(8), Arrays.asList(6, 6));
+
+        List<excercise.Problem10.IntegersEncoder> encodedList = new ArrayList<>();
+
+        for (List<Integer> numbers : list) {
+            encodedList.add(new excercise.Problem10.IntegersEncoder(numbers.size(), numbers.get(0).toString()));
+        }//Problem 10 code
+
+        List<Object> modList = new ArrayList<>();
+
+        for (IntegersEncoder integersEncoder : encodedList) {
+            if (integersEncoder.getAmount() == 1) {
+                modList.add(integersEncoder.getCharacter());
+            } else
+                modList.add(integersEncoder);
+        }
+
+
+    }
 }
