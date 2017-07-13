a0a0a1f20bf04dddd7c4a4a3863cc468b140717a
mloszek <miloszloszek@gmail.com>
2017-07-13 16:23:22 +0200

    next_commit;

--- src/main/java/excercise/Problem16/Main.java
+++ src/main/java/excercise/Problem16/Main.java
@@ -1,5 +1,6 @@
 package excercise.Problem16;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,12 +8,23 @@ import java.util.List;
  * Dropping every N'th element of a list
  */
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         List<String> list = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j");
 
+        List<String> result = new ArrayList<>();
 
+        int counter = 0;
 
+        for (int i = 0; i < list.size(); i++) {
+            if (counter < 2) {
+                result.add(list.get(i));
+                counter++;
+            }else
+                counter = 0;
+        }
+
+        result.forEach(System.out::println);
 
     }
 }
