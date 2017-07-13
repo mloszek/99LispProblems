d551a3007975ac43f3f97667e3401e67da3070a1
mloszek <miloszloszek@gmail.com>
2017-07-13 16:26:47 +0200

    next_commit; up to problem 17

---
+++ src/main/java/excercise/Problem17/Main.java
@@ -0,0 +1,17 @@
+package excercise.Problem17;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Splitting list into two parts
+ */
+public class Main {
+    public static void main(String[] args){
+
+        List<String> list = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h");
+
+
+
+    }
+}
