572b3af30a535716cb29dc1e2bc8d3b9aecb0436
mloszek <miloszloszek@gmail.com>
2017-07-13 16:34:32 +0200

    next_commit; up to problem 18

---
+++ src/main/java/excercise/Problem18/Main.java
@@ -0,0 +1,17 @@
+package excercise.Problem18;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Extract a slice from a list
+ */
+public class Main {
+    public static void main(String[] args){
+
+        List<String> list = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h");
+
+
+
+    }
+}
