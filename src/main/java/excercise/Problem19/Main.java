ecc71ceddd69d50011609f19cf1e0e9b60f2abad
mloszek <miloszloszek@gmail.com>
2017-07-14 14:28:12 +0200

    next_commit; up to problem 19

--- src/main/java/excercise/Problem19/Main.java
+++ src/main/java/excercise/Problem19/Main.java
@@ -1,11 +1,15 @@
 package excercise.Problem19;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Swapping a list N places to the left..
  */
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        List<String> list = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h");
 
 
 
